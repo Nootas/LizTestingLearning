1325e81be664eb61b24623f9c44886deff177a88
Nootas <39734768+Nootas@users.noreply.github.com>
2018-09-23 21:07:36 +0800

    Update taeread.py

--- learning/taeread.py
+++ learning/taeread.py
@@ -1,10 +1,12 @@
 '''
-PYthon IO
+python IO
 open 位于IO模块，但IO模块是自动导入的，无需人为导入
+应该是用with时，调用file.closed,不是用with时，使用file.close()
+这里是读取某一个文件的前三行内容
 '''
 
-with open(r'/Users/otae/Desktop/TaePython/something.txt') as file:
+with open(r'/path/something.txt') as file:
     i=0
     for i in range(3):
-        print(file.readline(),end='')
-    file.close()
+        print(file.readline(),end='') #加一个print用来输出读取到的内容
+    file.closed
