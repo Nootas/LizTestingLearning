aaf7c872975fe9ec2a36b90ff4af4933212458aa
Nootas <39734768+Nootas@users.noreply.github.com>
2018-11-25 14:35:54 +0800

    Update MyVariables.py

--- RobotFramework/WeiboTest/MyVariables.py
+++ RobotFramework/WeiboTest/MyVariables.py
@@ -3,5 +3,5 @@ import time
 import linecache
 
 CURRENT_TIME = time.asctime()  #timestamp
-MESSAGE = linecache.getline("doc.txt",1) #lineno start from 1
-#MESSAGE = linecache.getline("doc.txt",1).strip()
+MESSAGE = linecache.getline("weibocontent.txt",1) #lineno start from 1
+#MESSAGE = linecache.getline("weibocontent.txt",1).strip()
