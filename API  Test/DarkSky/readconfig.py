31a820eed8513272208ee977e8ceb8ea0fd3fd86
Nootas <39734768+Nootas@users.noreply.github.com>
2018-12-18 17:32:15 +0800

    remove something unnecessary param

--- API  Test/DarkSky/readconfig.py
+++ API  Test/DarkSky/readconfig.py
@@ -11,24 +11,24 @@ class ReadConfig:
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')  #read config file
 
-    def get_key(self,name):
-        key = self.config.get('SECRET KEY',name)
+    def get_key(self):
+        key = self.config.get('SECRET KEY','Key')
         return key
 
-    def get_host(self,name):
-        host = self.config.get('SERVER',name)
+    def get_host(self):
+        host = self.config.get('SERVER','Host')
         return host
 
-    def get_latitude(self,name):
-        latitude = self.config.get('LOCATION',name)
+    def get_latitude(self):
+        latitude = self.config.get('LOCATION','Latitude')
         return latitude
 
-    def get_longtitude(self,name):
-        longtitude = self.config.get('LOCATION',name)
+    def get_longtitude(self):
+        longtitude = self.config.get('LOCATION','Longtitude')
         return longtitude
 
-    def get_location(self,*args):
-        latitude = self.config.get('LOCATION',args[0])
-        longtitude = self.config.get('LOCATION',args[1])
+    def get_location(self):
+        latitude = self.config.get('LOCATION','Latitude')
+        longtitude = self.config.get('LOCATION','Longtitude')
         locationdict = {'latitude':float(latitude),'longtitude':float(longtitude)}
         return locationdict
